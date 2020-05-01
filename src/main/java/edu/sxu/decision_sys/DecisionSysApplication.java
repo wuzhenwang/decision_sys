9102a163c6dd0715911449f87e616e230adfd735
wuzhenwang <wuzhwang@163.com>
2020-05-01 13:51:19 +0800

    第一次提交

---
+++ src/main/java/edu/sxu/decision_sys/DecisionSysApplication.java
@@ -0,0 +1,13 @@
+package edu.sxu.decision_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecisionSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/sxu/decision_sys/DecisionSysApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sxu.decision_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
